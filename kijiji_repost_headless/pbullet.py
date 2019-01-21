b2d49232c51b2dbdca0ee57bffc6651574ac3c3b
Hatoris <florianxbernard@gmail.com>
2019-01-20 21:36:38 -0500

    add minor change to pbullet function

--- kijiji_repost_headless/pbullet.py
+++ kijiji_repost_headless/pbullet.py
@@ -1,10 +1,20 @@
-from typing import List
+from typing import List, Dict
 from pushbullet import Pushbullet, InvalidKeyError
  
+import yaml
+
+def get_pbullet_key(data : Dict):
+    """
+    If api key in .yml file return it else ruturn false
+    """
+    api_key = data.get("pbullet_key", False)
+    if api_key:
+        return api_key.strip()
+    return False
+
 def pbullet(
-        pb_api_key : str,
-        title : str,
-        body : str,
+        data : Dict,
+        body : List
         ) -> None:
         """
         Send notification via pushbullet
@@ -19,10 +29,20 @@ def pbullet(
         Return
         -----------
         None           
-        """          
-        try:
-            pb = Pushbullet(pb_api_key)
-            pb.push_note(title, body)
-            print("pushbullet notify with sucess !")           
-        except InvalidKeyError:
-             print("Your key is not valid")
+        """
+        pb_api_key = get_pbullet_key(data)
+        if pb_api_key:
+            corps = create_body(body)
+            title = data["postAdForm.title"]
+            try:
+                pb = Pushbullet(pb_api_key)
+                pb.push_note(title, corps)
+                print("pushbullet notify with sucess !")           
+            except InvalidKeyError:
+                print("Your key is not valid")
+
+def create_body(infos : List):
+    body = ""
+    for info in infos:
+        body += f'{info} \n'
+    return body
